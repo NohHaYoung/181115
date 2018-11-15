77293bd2fb167f7451e0921220d7ee94edcff1c9
NohHaYoung <42965065+NohHaYoung@users.noreply.github.com>
2018-11-15 16:25:38 +0900

    6

--- main.c
+++ main.c
@@ -3,52 +3,47 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-	int addfunc(int x, int y){
-		return x + y;
+void print_image(int image[][5]){
+	int i, j;
+	int *ptr = &image[0][0];
+	
+	for(i=0; i<5; i++){
+		for(j=0; j<5; j++){
+			printf("%d ", *ptr);
+			ptr = ptr + 1;
+		}printf("\n");
+	}printf("\n");
+}
+
+void brighten_image(int image[][5]){
+	int i=0, j=0;
+	int *ptr = &image[i][j];
+	
+	for(i=0;i<5;i++){
+		for(j=0;j<5;j++){
+			*ptr = *ptr +10;
+			 ptr = ptr +1;
+		}
 	}
 	
-	int subfunc(int x, int y){
-		return x - y;		
-	}
-	int mulfunc(int x, int y){
-		return x*y;
-	}
-	int divfunc(int x, int y){
-		return x/y;
-	}
+}
+
+
+
 
 int main(int argc, char *argv[]) {
 	
-	int num1;
-	int num2;
-	char op;
-	int (*calcfunc)(int, int);
+	int image[5][5] = {
+		{10, 20, 30, 40, 50},
+		{10, 20, 30, 40, 50},
+		{10, 20, 30, 40, 50},
+		{10, 20, 30, 40, 50},
+		{10, 20, 30, 40, 50}
+	};
 	
-	printf("Input Calculation : ");
-	scanf("%d %c %d", &num1, &op, &num2);
-	
-
-	
-	switch(op)
-		{
-			case '+' :
-				calcfunc = addfunc;
-				break;
-				
-			case '-' : 
-				calcfunc = subfunc;
-				break;
-				
-			case '*' :
-				calcfunc = mulfunc;
-				break;
-				
-			case '/' :
-				calcfunc = divfunc;
-				break;	
-		}
-		
-	printf("result : %i\n", calcfunc(num1, num2));
+	print_image(image);
+	brighten_image(image);
+	print_image(image);
 		
 	return 0;
 }
